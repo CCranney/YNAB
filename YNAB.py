65f4a99a48a0cd932347f88fc12276d0495a095f
CCran <mellowcello13@gmail.com>
2021-09-06 09:38:24 -0600

    begin add income test/implementation

--- YNAB.py
+++ YNAB.py
@@ -6,7 +6,15 @@ class YNAB:
     def __init__(self):
         if not os.path.isdir('data'):
             os.makedirs('data')
-            pd.DataFrame(data=[], columns=['Amount','Account','Memo', 'Month']).to_csv('data/income.csv', index=False)
+            pd.DataFrame(data=[], columns=['Amount','Account','Memo', 'Date']).to_csv('data/income.csv', index=False)
             pd.DataFrame(data=[], columns=['TargetCategory','PriorCategory','Amount','Month']).to_csv('data/assignments.csv', index=False)
             pd.DataFrame(data=[], columns=['Category','Amount','EndMonth']).to_csv('data/regularPayments.csv', index=False)
-            pd.DataFrame(data=[], columns=['Category','Amount','Account','Month']).to_csv('data/expenses.csv', index=False)
+            pd.DataFrame(data=[], columns=['Category','Amount','Account','Date']).to_csv('data/expenses.csv', index=False)
+        self.income = pd.read_csv('data/income.csv')
+        self.assignments = pd.read_csv('data/assignments.csv')
+        self.regularPayments = pd.read_csv('data/regularPayments.csv')
+        self.expenses = pd.read_csv('data/expenses.csv')
+
+    def add_income(self, amount, account, memo, date):
+        self.income.loc[len(self.income.index)] = [amount, account, memo, date]
+        self.income.to_csv('data/income.csv')
--- tests_ONLY_RUN_ON_TEST_DIRECTORY.py
+++ tests_ONLY_RUN_ON_TEST_DIRECTORY.py
@@ -18,7 +18,7 @@ class MyTest(unittest.TestCase):
 
         self.assertTrue(os.path.isfile('data/income.csv'))
         income = pd.read_csv('data/income.csv')
-        self.assertEqual(set(income.columns), set(['Amount','Account','Memo', 'Month']))
+        self.assertEqual(set(income.columns), set(['Amount','Account','Memo', 'Date']))
 
         self.assertTrue(os.path.isfile('data/assignments.csv'))
         assignments = pd.read_csv('data/assignments.csv')
@@ -30,7 +30,27 @@ class MyTest(unittest.TestCase):
 
         self.assertTrue(os.path.isfile('data/expenses.csv'))
         expenses = pd.read_csv('data/expenses.csv')
-        self.assertEqual(set(expenses.columns), set(['Category','Amount','Account','Month']))
+        self.assertEqual(set(expenses.columns), set(['Category','Amount','Account','Date']))
+
+        self.assertTrue(hasattr(self.YNAB, 'income'))
+        self.assertTrue(hasattr(self.YNAB, 'assignments'))
+        self.assertTrue(hasattr(self.YNAB, 'regularPayments'))
+        self.assertTrue(hasattr(self.YNAB, 'expenses'))
+
+    def test_data_input_income(self):
+        self.YNAB = YNAB.YNAB()
+        #self.assertRaises()
+        #dates: https://stackabuse.com/how-to-format-dates-in-python/
+        self.YNAB.add_income(1.00, 'UCCU', 'memo', 'date')
+        tempIncome = pd.read_csv('data/income.csv')
+        self.assertEqual(self.YNAB.income.loc[0]['Amount'],1.00)
+        self.assertEqual(tempIncome.loc[0]['Amount'],1.00)
+        self.assertEqual(self.YNAB.income.loc[0]['Account'],'UCCU')
+        self.assertEqual(tempIncome.loc[0]['Account'],'UCCU')
+        self.assertEqual(self.YNAB.income.loc[0]['Memo'],'memo')
+        self.assertEqual(tempIncome.loc[0]['Memo'],'memo')
+        self.assertEqual(self.YNAB.income.loc[0]['Date'],'date')
+        self.assertEqual(tempIncome.loc[0]['Date'],'date')
 
 
 if __name__ == '__main__':
